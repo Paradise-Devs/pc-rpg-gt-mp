a2c22034d63dab5b9e318bca079ab21eef6aeaa6
Lucas Godoy <lcsdegodoy@outlook.com>
2017-07-26 18:50:47 -0300

    Adiciona lista e seleção de roupas masculinas

--- src/Player/Character/CharacterCreator.ts
+++ src/Player/Character/CharacterCreator.ts
@@ -3,7 +3,7 @@
 var browser = null;
 
 // Character's Data
-var characterData = {
+var character_data = {
     gender: 0,
     faceFirst: 0,
     faceSecond: 0,
@@ -29,6 +29,8 @@ var characterData = {
     feet: 1,
     undershirt: 57,
     topshirt: 1,
+    topshirtTexture: 0,
+    accessory: 0,
 };
 
 API.onResourceStop.connect(() =>
@@ -42,7 +44,35 @@ API.onResourceStop.connect(() =>
 
 function ChangeCharacterGender(id)
 {
-    characterData.gender = id;
+    character_data.gender = id;
+
+    character_data.faceFirst = 0;
+    character_data.faceSecond = 0;
+    character_data.faceMix = 0.0;
+    character_data.skinFirst = 0;
+    character_data.skinSecond = 0;
+    character_data.skinMix = 0.0;
+    character_data.hairType = 0;
+    character_data.hairColor = 0;
+    character_data.hairHighlight = 0;
+    character_data.eyeColor = 0;
+    character_data.eyebrows = 0;
+    character_data.eyebrowsColor1 = 0;
+    character_data.eyebrowsColor2 = 0;
+    character_data.beard = null;
+    character_data.beardColor = 0;
+    character_data.makeup = null;
+    character_data.makeupColor = 0;
+    character_data.lipstick = null;
+    character_data.lipstickColor = 0;
+    character_data.torso = 0;
+    character_data.legs = 1;
+    character_data.feet = 1;
+    character_data.undershirt = 57;
+    character_data.topshirt = 1;
+    character_data.topshirtTexture = 0;
+    character_data.accessory = 0;
+
     ResetCharacterCreation();
 }
 
@@ -67,36 +97,37 @@ function ShowCharacterCreator()
 
 function ResetCharacterCreation()
 {
-    characterData.torso = characterData.gender ? 5 : 0;
-    characterData.undershirt = characterData.gender ? 95 : 57;
-    characterData.hairType = characterData.gender ? 4 : 0;    
+    character_data.torso = character_data.gender ? 5 : 0;
+    character_data.undershirt = character_data.gender ? 95 : 57;
+    character_data.hairType = character_data.gender ? 4 : 0;    
 
-    API.setPlayerSkin(characterData.gender ? -1667301416 : 1885233650);
+    API.setPlayerSkin(character_data.gender ? -1667301416 : 1885233650);
 
-    API.callNative("SET_PED_HEAD_BLEND_DATA", API.getLocalPlayer(), characterData.faceFirst, characterData.faceSecond, 0, characterData.skinFirst, characterData.skinSecond, 0, characterData.faceMix, characterData.skinMix, 0, false);
+    API.callNative("SET_PED_HEAD_BLEND_DATA", API.getLocalPlayer(), character_data.faceFirst, character_data.faceSecond, 0, character_data.skinFirst, character_data.skinSecond, 0, character_data.faceMix, character_data.skinMix, 0, false);
 
-    API.setPlayerClothes(API.getLocalPlayer(), 2, characterData.hairType, 0);
-    API.callNative("_SET_PED_HAIR_COLOR", API.getLocalPlayer(), characterData.hairColor, characterData.hairHighlight);
+    API.setPlayerClothes(API.getLocalPlayer(), 2, character_data.hairType, 0);
+    API.callNative("_SET_PED_HAIR_COLOR", API.getLocalPlayer(), character_data.hairColor, character_data.hairHighlight);
 
-    API.callNative("_SET_PED_EYE_COLOR", API.getLocalPlayer(), characterData.eyeColor);
+    API.callNative("_SET_PED_EYE_COLOR", API.getLocalPlayer(), character_data.eyeColor);
 
-    API.callNative("SET_PED_HEAD_OVERLAY", API.getLocalPlayer(), 2, characterData.eyebrows, API.f(1));
-    API.callNative("_SET_PED_HEAD_OVERLAY_COLOR", API.getLocalPlayer(), 2, 1, characterData.eyebrowsColor1, characterData.eyebrowsColor2);
+    API.callNative("SET_PED_HEAD_OVERLAY", API.getLocalPlayer(), 2, character_data.eyebrows, API.f(1));
+    API.callNative("_SET_PED_HEAD_OVERLAY_COLOR", API.getLocalPlayer(), 2, 1, character_data.eyebrowsColor1, character_data.eyebrowsColor2);
 
-    API.callNative("SET_PED_HEAD_OVERLAY", API.getLocalPlayer(), 1, characterData.beard == null ? 0 : characterData.beard, API.f(characterData.beard == null ? 0 : 1));
-    API.callNative("_SET_PED_HEAD_OVERLAY_COLOR", API.getLocalPlayer(), 1, 1, characterData.beardColor, characterData.beardColor);
+    API.callNative("SET_PED_HEAD_OVERLAY", API.getLocalPlayer(), 1, character_data.beard == null ? 0 : character_data.beard, API.f(character_data.beard == null ? 0 : 1));
+    API.callNative("_SET_PED_HEAD_OVERLAY_COLOR", API.getLocalPlayer(), 1, 1, character_data.beardColor, character_data.beardColor);
 
-    API.callNative("SET_PED_HEAD_OVERLAY", API.getLocalPlayer(), 4, characterData.makeup == null ? 0 : characterData.makeup, API.f(characterData.makeup == null ? 0 : 1));
-    API.callNative("_SET_PED_HEAD_OVERLAY_COLOR", API.getLocalPlayer(), 4, 0, characterData.makeupColor, characterData.makeupColor);
+    API.callNative("SET_PED_HEAD_OVERLAY", API.getLocalPlayer(), 4, character_data.makeup == null ? 0 : character_data.makeup, API.f(character_data.makeup == null ? 0 : 1));
+    API.callNative("_SET_PED_HEAD_OVERLAY_COLOR", API.getLocalPlayer(), 4, 0, character_data.makeupColor, character_data.makeupColor);
 
-    API.callNative("SET_PED_HEAD_OVERLAY", API.getLocalPlayer(), 8, characterData.lipstick == null ? 0 : characterData.lipstick, API.f(characterData.lipstick == null ? 0 : 1));
-    API.callNative("_SET_PED_HEAD_OVERLAY_COLOR", API.getLocalPlayer(), 8, 2, characterData.lipstickColor, characterData.lipstickColor);
+    API.callNative("SET_PED_HEAD_OVERLAY", API.getLocalPlayer(), 8, character_data.lipstick == null ? 0 : character_data.lipstick, API.f(character_data.lipstick == null ? 0 : 1));
+    API.callNative("_SET_PED_HEAD_OVERLAY_COLOR", API.getLocalPlayer(), 8, 2, character_data.lipstickColor, character_data.lipstickColor);
 
-    API.setPlayerClothes(API.getLocalPlayer(), 3, characterData.torso, 0);
-    API.setPlayerClothes(API.getLocalPlayer(), 4, characterData.legs, 0);
-    API.setPlayerClothes(API.getLocalPlayer(), 6, characterData.feet, 0);
-    API.setPlayerClothes(API.getLocalPlayer(), 8, characterData.undershirt, 0);
-    API.setPlayerClothes(API.getLocalPlayer(), 11, characterData.topshirt, 0);
+    API.setPlayerClothes(API.getLocalPlayer(), 3, character_data.torso, 0);
+    API.setPlayerClothes(API.getLocalPlayer(), 4, character_data.legs, 0);
+    API.setPlayerClothes(API.getLocalPlayer(), 6, character_data.feet, 0);
+    API.setPlayerClothes(API.getLocalPlayer(), 7, character_data.accessory, 0);
+    API.setPlayerClothes(API.getLocalPlayer(), 8, character_data.undershirt, 0);
+    API.setPlayerClothes(API.getLocalPlayer(), 11, character_data.topshirt, 0);
 }
 
 function MoveCameraPosition(pos)
@@ -116,10 +147,10 @@ function MoveCameraPosition(pos)
         case 1:
             {
                 // Torso
-                var camPos = new Vector3(402.9378, -997.5, -98.90);
+                var camPos = new Vector3(402.9378, -997.5, -98.60);
                 var camRot = new Vector3(0.0, 0.0, 176.891);
                 var camera = API.createCamera(camPos, camRot);
-                API.pointCameraAtPosition(camera, new Vector3(402.9198, -996.5348, -98.90));
+                API.pointCameraAtPosition(camera, new Vector3(402.9198, -996.5348, -98.60));
                 API.interpolateCameras(API.getActiveCamera(), camera, 2000, false, false);
                 break;
             }
@@ -165,4 +196,33 @@ function GoBackToSelection()
     }
     MoveCameraPosition(-1);
     resource.CharacterSelector.ShowCharacterSelector();
+}
+
+function SelectCharacterComponent(data)
+{
+    data = JSON.parse(data);
+
+    if (data.torso != undefined)
+    {
+        character_data.torso = data.torso;
+        API.setPlayerClothes(API.getLocalPlayer(), 3, data.torso, 0);
+    }
+
+    if (data.undershirt != undefined)
+    {
+        character_data.undershirt = data.undershirt;
+        API.setPlayerClothes(API.getLocalPlayer(), 8, data.undershirt, 0);
+    }
+
+    if (data.slot == 4) character_data.legs = data.variation;
+    else if (data.slot == 6) character_data.feet = data.variation;
+    else if (data.slot == 7) character_data.accessory = data.variation;
+    else if (data.slot == 8) character_data.undershirt = data.variation;
+    else if (data.slot == 11)
+    {        
+        character_data.topshirt = data.variation;
+        character_data.topshirtTexture = data.texture;
+    }
+
+    API.setPlayerClothes(API.getLocalPlayer(), data.slot, data.variation, data.texture);
 }

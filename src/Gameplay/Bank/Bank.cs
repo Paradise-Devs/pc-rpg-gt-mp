7d2092e351a0e9a4c62b764353e65a48c7a44ca5
Lucas Godoy <lcsdegodoy@outlook.com>
2018-01-03 18:50:25 -0200

    Adiciona banco/caixa eletrônico dinâmicos

---
+++ src/Gameplay/Bank/Bank.cs
@@ -0,0 +1,179 @@
+﻿using GrandTheftMultiplayer.Server.API;
+using GrandTheftMultiplayer.Server.Elements;
+using GrandTheftMultiplayer.Server.Managers;
+using GrandTheftMultiplayer.Shared.Math;
+using Newtonsoft.Json;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+
+namespace pcrpg.src.Gameplay.Bank
+{
+    #region Bank Class
+    public class Bank
+    {
+        public Guid ID { get; }
+        public Vector3 Position { get; }
+        public int Type { get; private set; }
+
+        // entities
+        [JsonIgnore]
+        private Blip Blip;
+
+        [JsonIgnore]
+        private Marker Marker;
+
+        [JsonIgnore]
+        private ColShape ColShape;
+
+        [JsonIgnore]
+        private DateTime LastSave;
+
+        public Bank(Guid id, Vector3 position, int type)
+        {
+            ID = id;
+            Position = position;
+            Type = type;
+
+            // create blip
+            if (Type == 0)
+            {
+                Blip = API.shared.createBlip(position);
+                Blip.name = "Banco";
+                Blip.scale = 1f;
+                Blip.shortRange = true;
+                UpdateBlip();
+            }
+
+            // create colshape
+            ColShape = API.shared.createCylinderColShape(position, 0.85f, 0.85f);
+            ColShape.onEntityEnterColShape += (s, ent) =>
+            {
+                Client player;
+
+                if ((player = API.shared.getPlayerFromHandle(ent)) != null)
+                {
+                    player.setData("BankMarker_ID", ID);
+                }
+            };
+
+            ColShape.onEntityExitColShape += (s, ent) =>
+            {
+                Client player;
+
+                if ((player = API.shared.getPlayerFromHandle(ent)) != null)
+                {
+                    player.resetData("BankMarker_ID");
+                }
+            };
+
+            // create marker
+            Marker = API.shared.createMarker(1, position - new Vector3(0.0, 0.0, 1.0), new Vector3(), new Vector3(), new Vector3(0.5, 0.5, 0.25), 150, 25, 255, 25);
+        }
+
+        private void UpdateBlip()
+        {
+            if (!Blip.exists) return;
+
+            Blip.sprite = 108;
+            Blip.color = 69;
+        }
+
+        public void SetType(int new_type)
+        {
+            Type = new_type;
+
+            if (new_type == 1 && API.shared.doesEntityExist(Blip)) Blip.delete();
+            else if (new_type == 0 && !API.shared.doesEntityExist(Blip))
+            {
+                Blip = API.shared.createBlip(Position);
+                Blip.name = "Banco";
+                Blip.scale = 1f;
+                Blip.shortRange = true;
+                UpdateBlip();
+            }
+
+            Save();
+        }
+
+        public void Save(bool force = false)
+        {
+            if (!force && DateTime.Now.Subtract(LastSave).TotalSeconds < Main.SAVE_INTERVAL) return;
+
+            File.WriteAllText(Main.BANK_SAVE_DIR + Path.DirectorySeparatorChar + ID + ".json", JsonConvert.SerializeObject(this, Formatting.Indented));
+            LastSave = DateTime.Now;
+        }
+
+        public void Destroy()
+        {
+            if (Blip.exists) Blip.delete();
+            Marker.delete();
+            API.shared.deleteColShape(ColShape);
+        }
+    }
+    #endregion
+
+    public class Main : Script
+    {
+        // settings
+        public static string BANK_SAVE_DIR = "data/Bank";
+        public static int SAVE_INTERVAL = 120;
+
+        public static List<Bank> Banks = new List<Bank>();
+
+        public Main()
+        {
+            API.onResourceStart += Bank_Init;
+            API.onResourceStop += Bank_Exit;
+        }
+
+        #region Methods
+        public static Guid GetGuid()
+        {
+            Guid new_guid;
+
+            do
+            {
+                new_guid = Guid.NewGuid();
+            } while (Banks.Count(h => h.ID == new_guid) > 0);
+
+            return new_guid;
+        }
+        #endregion
+
+        #region Events
+        public void Bank_Init()
+        {
+            // load settings
+            if (API.hasSetting("bankDirName")) BANK_SAVE_DIR = API.getSetting<string>("bankDirName");
+
+            BANK_SAVE_DIR = API.getResourceFolder() + Path.DirectorySeparatorChar + BANK_SAVE_DIR;
+            if (!Directory.Exists(BANK_SAVE_DIR)) Directory.CreateDirectory(BANK_SAVE_DIR);
+
+            if (API.hasSetting("saveInterval")) SAVE_INTERVAL = API.getSetting<int>("saveInterval");
+            API.consoleOutput("-> Save Interval: {0}", TimeSpan.FromSeconds(SAVE_INTERVAL).ToString(@"hh\:mm\:ss"));
+
+            // load parking lots
+            foreach (string file in Directory.EnumerateFiles(BANK_SAVE_DIR, "*.json"))
+            {
+                Bank bank = JsonConvert.DeserializeObject<Bank>(File.ReadAllText(file));
+                Banks.Add(bank);
+            }
+
+            API.consoleOutput("Loaded {0} banks/atms.", Banks.Count);
+        }
+
+        public void Bank_Exit()
+        {
+            foreach (Bank bank in Banks)
+            {
+                bank.Save(true);
+                bank.Destroy();
+            }
+
+            Banks.Clear();
+        }
+        #endregion
+    }
+}
---
+++ src/Gameplay/Bank/Commands.cs
@@ -0,0 +1,98 @@
+﻿using GrandTheftMultiplayer.Server.API;
+using GrandTheftMultiplayer.Server.Elements;
+using GrandTheftMultiplayer.Server.Managers;
+using pcrpg.src.Admin;
+using System.IO;
+using System.Linq;
+
+namespace pcrpg.src.Gameplay.Bank
+{
+    class Commands : Script
+    {
+        [Command("bankcmds", Alias = "bcmds")]
+        public void CommandsCommand(Client player)
+        {
+            if (!player.IsAdmin())
+            {
+                player.sendChatMessage("~r~ERRO: ~w~Você não tem permissão.");
+                return;
+            }
+
+            API.sendChatMessageToPlayer(player, "~p~~~~~~~~~~~~~~~~~~ Comandos Banco ~~~~~~~~~~~~~~~~~");
+            API.sendChatMessageToPlayer(player, "* /createbank - /deletebank - /setbanktype");
+            API.sendChatMessageToPlayer(player, "~p~~~~~~~~~~~~~~~~~~ Comandos Banco ~~~~~~~~~~~~~~~~~");
+        }
+
+        [Command("createbank")]
+        public void CMD_CreateBank(Client player, int type)
+        {
+            if (!player.IsAdmin())
+            {
+                player.sendChatMessage("~r~ERRO: ~w~Você não tem permissão.");
+                return;
+            }
+            
+            else if (type < 0 || type > 1)
+            {
+                player.sendChatMessage("~r~ERRO: ~w~Use valores entre 0 e 1 para o tipo.");
+                return;
+            }
+
+            Bank new_bank = new Bank(Main.GetGuid(), player.position, type);
+            new_bank.Save();
+
+            Main.Banks.Add(new_bank);
+            API.sendChatMessageToPlayer(player, (type == 0) ? "~g~SUCESSO: ~s~Você adicionou um banco em sua posição." : "~g~SUCESSO: ~s~Você adicionou um caixa eletrônico em sua posição.");
+        }
+
+        [Command("deletebank")]
+        public void CMD_RemoveBank(Client player)
+        {
+            if (!player.IsAdmin())
+            {
+                player.sendChatMessage("~r~ERRO: ~w~Você não tem permissão.");
+                return;
+            }
+
+            if (!player.hasData("BankMarker_ID"))
+            {
+                player.sendChatMessage("~r~ERRO: ~w~Fique em cima da posição do banco que deseja excluir.");
+                return;
+            }
+
+            Bank bank = Main.Banks.FirstOrDefault(h => h.ID == player.getData("BankMarker_ID"));
+            if (bank == null) return;
+
+            bank.Destroy();
+            Main.Banks.Remove(bank);
+
+            string bank_file = Main.BANK_SAVE_DIR + Path.DirectorySeparatorChar + bank.ID + ".json";
+            if (File.Exists(bank_file)) File.Delete(bank_file);
+            API.sendChatMessageToPlayer(player, (bank.Type == 0) ? "~g~SUCESSO: ~s~Você deletou este banco." : "~g~SUCESSO: ~s~Você deletou este caixa eletrônico.");
+        }
+
+        [Command("setbanktype")]
+        public void CMD_SetBankType(Client player, int new_type)
+        {
+            if (!player.IsAdmin())
+            {
+                player.sendChatMessage("~r~ERRO: ~w~Você não tem permissão.");
+                return;
+            }
+
+            if (!player.hasData("BankMarker_ID"))
+            {
+                player.sendChatMessage("~r~ERRO: ~w~Fique em cima da posição do banco que deseja editar.");
+                return;
+            }
+
+            Bank bank = Main.Banks.FirstOrDefault(h => h.ID == player.getData("BankMarker_ID"));
+            if (bank == null) return;
+
+            bank.SetType(new_type);
+            bank.Save();
+
+            API.sendChatMessageToPlayer(player, (new_type == 0) ? "~g~SUCESSO: ~s~Você alterou o tipo para banco." : "~g~SUCESSO: ~s~Você alterou o tipo para caixa eletrônico.");
+        }
+    }
+}

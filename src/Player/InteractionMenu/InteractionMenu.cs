0b30fead4531cf7ffe9b0b3ef58a6c9b2f309211
Lucas Godoy <lcsdegodoy@outlook.com>
2018-01-06 19:52:27 -0200

    Adiciona menu de interação

---
+++ src/Player/InteractionMenu/InteractionMenu.cs
@@ -0,0 +1,74 @@
+﻿using GrandTheftMultiplayer.Server.API;
+using GrandTheftMultiplayer.Server.Elements;
+using GrandTheftMultiplayer.Shared.Math;
+using pcrpg.src.Player.Utils;
+
+namespace pcrpg.src.Player.InteractionMenu
+{
+    class InteractionMenu : Script
+    {
+        public InteractionMenu()
+        {
+            API.onClientEventTrigger += OnClientEvent;
+        }
+
+        private void OnClientEvent(Client sender, string eventName, params object[] arguments)
+        {
+            switch (eventName)
+            {
+                case "InteractionMenu_GetWaypointPosition":
+                    int index = (int)arguments[0];
+                    Vector3 position = null;
+                    switch (index)
+                    {
+                        case 1:
+                            {
+                                float distance = -1f;
+                                foreach (var bank in Gameplay.Bank.Main.Banks)
+                                {
+                                    if (bank.Position.DistanceTo(sender.position) < distance || distance < 0f)
+                                    {
+                                        position = bank.Position;
+                                        distance = bank.Position.DistanceTo(sender.position);
+                                    }
+                                }
+                                break;
+                            }
+                        case 2:
+                            {
+                                float distance = -1f;
+                                foreach (var dealership in Gameplay.Dealership.Main.Dealerships)
+                                {
+                                    if (dealership.Position.DistanceTo(sender.position) < distance || distance < 0f)
+                                    {
+                                        position = dealership.Position;
+                                        distance = dealership.Position.DistanceTo(sender.position);
+                                    }
+                                }
+                                break;
+                            }
+                        case 3:
+                            {
+                                float distance = -1f;
+                                foreach (var parkinglot in Gameplay.Parkinglot.Main.Parkinglots)
+                                {
+                                    if (parkinglot.Position.DistanceTo(sender.position) < distance || distance < 0f)
+                                    {
+                                        position = parkinglot.Position;
+                                        distance = parkinglot.Position.DistanceTo(sender.position);
+                                    }
+                                }
+                                break;
+                            }
+                    }
+
+                    if (position != null) sender.triggerEvent("InteractionMenu_SetWaypointPosition", position.X, position.Y);
+                    break;
+                case "InteractionMenu_Suicide":
+                    sender.sendChatAction("cometeu suicídio.");
+                    sender.kill();
+                    break;
+            }
+        }
+    }
+}
---
+++ src/Player/InteractionMenu/InteractionMenu.ts
@@ -0,0 +1,56 @@
+﻿/// <reference path='../../../types-gt-mp/Definitions/index.d.ts' />
+
+var menu = null;
+
+API.onKeyDown.connect((sender: any, e: System.Windows.Forms.KeyEventArgs) => {
+    if (e.KeyCode === Keys.M) {
+        if (menu == null)
+            createInteractionMenu();
+
+        if (menu.Visible)
+            menu.Visible = false;
+        else {
+            if (API.isAnyMenuOpen()) return;
+            menu.Visible = true;
+        }
+    }
+});
+
+API.onServerEventTrigger.connect((eventName: string, args: System.Array<any>) => {
+    if (eventName == "InteractionMenu_SetWaypointPosition") {
+        API.setWaypoint(args[0], args[1]);
+    }
+});
+
+function createInteractionMenu() {
+    menu = API.createMenu(API.getPlayerName(API.getLocalPlayer()), "Menu para acesso rápido", 0, 0, 6);
+
+    // GPS
+    var list = new List(String);
+    list.Add("Nenhum");
+    list.Add("Banco");
+    list.Add("Concessionária");
+    list.Add("Estacionamento");
+    var list_item = API.createListItem("GPS Rápido", "Selecione para marcar uma localização no mapa.", list, 0);
+
+    list_item.OnListChanged.connect(function (sender, new_index) {
+        switch (new_index) {
+            case 0:
+                API.removeWaypoint();
+                break;
+            default:
+                API.triggerServerEvent("InteractionMenu_GetWaypointPosition", new_index);
+                break
+        }
+    });
+
+    menu.AddItem(list_item);
+
+    // Suicide
+    var temp_item = API.createMenuItem("Cometer suicídio", "Tem certeza que deseja fazer isso?");
+    temp_item.Activated.connect(function (sender, item) {
+        menu.Visible = false;
+        API.triggerServerEvent("InteractionMenu_Suicide");
+    });
+    menu.AddItem(temp_item);
+}
